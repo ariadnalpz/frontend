5d94c5510a0f2a5b4de26eb3e6ce408e17ac5a35
ariadnalpz <ariadna.lopez2904@gmail.com>
2025-04-02 15:37:40 -0600

    Axios server2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://servidor1-r4cq.onrender.com/api', // Cambia a 3002 si pruebas Servidor 2
+  baseURL: 'https://servidor2-r4cq.onrender.com/api', // Cambia a 3002 si pruebas Servidor 2
 });
 
 export default instance;
