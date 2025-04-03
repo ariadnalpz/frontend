ae17bda46ea4db9d194d281ad9082e3884a8e93f
ariadnalpz <ariadna.lopez2904@gmail.com>
2025-04-03 15:01:18 -0600

    Revertir

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://servidor2-6ldn.onrender.com', 
+  baseURL: 'https://servidor1-r4cq.onrender.com/api', 
 });
 
 // Interceptor para agregar el token a las solicitudes
